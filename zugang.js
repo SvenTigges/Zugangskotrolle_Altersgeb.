58a3c88e9f773473ea03aa11e8611ff9f8b541f5
SvenTigges <sven.tigges@web.de>
2021-05-21 08:45:47 +0200

    add functon(1)

---
+++ zugang.js
@@ -0,0 +1,40 @@
+// let firstName = prompt("Bitte Name eingenen: ");
+// let birthYear = parseInt(prompt("Bitte Geburtsjahr eingeben: "));
+// let year = new Date().getFullYear();
+//let entry = year - birthYear
+
+/*
+switch (true) {
+    case (entry>18 && entry<150):
+        console.log(firstName + " ist alt genug somit Zutritt erteilt");
+        break;
+    case (entry<17):
+        console.log("Leider zu Jung Zutritt Verweigert");
+        break;
+
+    default:
+        console.log("Fehlerhafte Eingabe");
+        break;
+}
+*/
+function zugang() 
+{
+    let firstName = prompt("Bitte Name eingenen: ");
+    let birthYear = parseInt(prompt("Bitte Geburtsjahr eingeben: "));
+    let year = new Date().getFullYear(); 
+
+    switch (true) 
+    {
+        case (((year - birthYear)>18) && ( (year - birthYear)<150)):
+            console.log(firstName + " ist alt genug somit Zutritt erteilt");
+            break;
+        case ((year - birthYear)<17):
+            console.log("Leider zu Jung Zutritt Verweigert");
+            break;
+    
+        default:
+            console.log("Fehlerhafte Eingabe");
+            break;
+    }
+}
+zugang();
