7431bc1ef5cdf2de3db5910c3485ee0cbf8c9c0f
SvenTigges <sven.tigges@web.de>
2021-05-22 23:31:01 +0200

    Funktionen

--- zugang.js
+++ zugang.js
@@ -1,7 +1,7 @@
 // let firstName = prompt("Bitte Name eingenen: ");
 // let birthYear = parseInt(prompt("Bitte Geburtsjahr eingeben: "));
 // let year = new Date().getFullYear();
-//let entry = year - birthYear
+// let entry = year - birthYear
 
 /*
 switch (true) {
@@ -17,24 +17,61 @@ switch (true) {
         break;
 }
 */
-function zugang() 
+zugang();
+function zugang()
 {
-    let firstName = prompt("Bitte Name eingenen: ");
-    let birthYear = parseInt(prompt("Bitte Geburtsjahr eingeben: "));
-    let year = new Date().getFullYear(); 
+  ausgabe(alterBerechnung(datum(geburtsJahr(Name))));  
+}
 
-    switch (true) 
+function alterBerechnung() 
+{
+    if (datum - geburtsJahr == 18)
     {
-        case (((year - birthYear)>18) && ( (year - birthYear)<150)):
-            console.log(firstName + " ist alt genug somit Zutritt erteilt");
-            break;
-        case ((year - birthYear)<17):
-            console.log("Leider zu Jung Zutritt Verweigert");
-            break;
-    
-        default:
-            console.log("Fehlerhafte Eingabe");
-            break;
+        return (firstName + " ist alt genug somit Zutritt erteilt");
+    } 
+    else 
+    {
+        return ("Leider zu Jung Zutritt Verweigert");
     }
 }
-zugang();
+    /*
+    switch (true) 
+    {
+        case ((( datum - geburtsJahr)>18) && ((datum - geburtsJahr)<150)):
+            return (firstName + " ist alt genug somit Zutritt erteilt");
+            
+        case ((datum - geburtsJahr)<17):
+            return ("Leider zu Jung Zutritt Verweigert");
+            
+        default:
+            return ("Fehlerhafte Eingabe");
+            
+    }   
+}
+*/
+
+//ausgabe(datum()); //check
+function datum() 
+{
+    let year = new Date().getFullYear();
+    return year;     
+}
+//ausgabe(geburtsJahr()); // check
+function geburtsJahr()
+{
+    let birthYear = parseInt(prompt("Bitte Geburtsjahr eingeben: "));
+    return birthYear;
+}
+//ausgabe(Name()); // check
+function Name() 
+{
+    let firstName = prompt("Bitte Name eingenen: ");
+    return firstName;
+}   
+
+//ausgabe("moin"); // check
+function ausgabe(outPut) 
+{
+    console.log(outPut);    
+}
+
